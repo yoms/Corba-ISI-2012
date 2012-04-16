b8d81d277a3141bb6aa68635d6d0fc68ccac7259
Guillaume Hormiere <hormiere.guillaume@gmail.com>
2012-04-16 16:33:39 +0200

    Fix minor database bug

--- src/fr/corba/server/DataBase.java
+++ src/fr/corba/server/DataBase.java
@@ -2,7 +2,7 @@ package fr.corba.server;
 import java.sql.*;
 
 public class DataBase {
-
+	Connection conn;
     public DataBase() {
 	    try {
 			Class.forName("org.sqlite.JDBC");
@@ -13,7 +13,6 @@ public class DataBase {
 		}
 
 	}
-	Connection conn;
 	private boolean createDataBase()
 	{
 	    try {
@@ -24,6 +23,7 @@ public class DataBase {
 		    ResultSet rs = verifstat.executeQuery("select * from users where nickname = 'yoms';");
 		    if(!rs.next())
 		    {
+		    	System.out.println("Save base users");
 			    PreparedStatement prep = conn.prepareStatement(
 			      "insert into users values (?, ?);");
 		
@@ -37,6 +37,8 @@ public class DataBase {
 			    prep.executeBatch();
 				conn.setAutoCommit(true);
 		    }
+		    verifstat.close();
+		    stat.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -48,9 +50,11 @@ public class DataBase {
 	    try {
 		    Statement stat = conn.createStatement();
 		    ResultSet rs = stat.executeQuery("select * from users where nickname = '"+nick+"' and password = '"+password+"';");
-		    if(rs.next())
-		    	return true;
+		    boolean ret = rs.next();
+		    stat.close();
+		    return ret;
 		} catch (Exception e) {
+			System.out.println();
 			e.printStackTrace();
 		} 
 	    return false;
