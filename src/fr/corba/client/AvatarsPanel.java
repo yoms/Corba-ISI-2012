09cac5013a98defc5e56e3d10f0d46dd0968ac63
Guillaume Hormiere <hormiere.guillaume@gmail.com>
2012-04-24 18:15:22 +0200

    improve admin hmi

--- src/fr/corba/client/AvatarsPanel.java
+++ src/fr/corba/client/AvatarsPanel.java
@@ -1,13 +1,51 @@
 package fr.corba.client;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+
+import java.awt.GridLayout;
+import java.awt.Label;
+import javax.swing.table.DefaultTableModel;
 
 public class AvatarsPanel extends JPanel {
+	private JTable table;
 
 	/**
 	 * Create the panel.
 	 */
 	public AvatarsPanel() {
+		setLayout(new GridLayout(0, 1, 0, 0));
+		table = new JTable();
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"Pseudo", "Taille", "Humeur", "Piece"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				true, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		add(scrollPane);
 
 	}
 
--- src/fr/corba/client/VirtualWorld.java
+++ src/fr/corba/client/VirtualWorld.java
@@ -1,6 +1,13 @@
 package fr.corba.client;
 
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.Canvas;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.JSplitPane;
+import java.awt.Component;
 
 public class VirtualWorld extends JPanel {
 
@@ -8,6 +15,28 @@ public class VirtualWorld extends JPanel {
 	 * Create the panel.
 	 */
 	public VirtualWorld() {
+		setLayout(new GridLayout(1, 0, 0, 0));
+		
+		Canvas canvas = new Canvas();
+		add(canvas);
+		
+		JPanel panel = new JPanel();
+		add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel pieceName = new JLabel("Unset");
+		pieceName.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(pieceName);
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JList portesList = new JList();
+		panel_1.add(portesList);
+		
+		JList avatarsList = new JList();
+		panel_1.add(avatarsList);
 
 	}
 
